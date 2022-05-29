0bac07676b6269497e16d0518b0de68ee185788f
saba <105594163+SabaH4@users.noreply.github.com>
2022-05-29 21:46:08 +0500

    Add files via upload

---
+++ Escape.java
@@ -0,0 +1,13 @@
+public class Escape 
+{
+  public static void main (String args[])
+  {
+System.out.println("yo\nfolks");
+System.out.println("hi\rbye");
+System.out.println("four\b4");
+System.out.println("fifty\tsixty\tseventy");
+System.out.println("\"Saba\"");
+System.out.println("\'A\'");
+System.out.println("\\NFC\\");
+  }
+}
